27a0245da27fcf9174ac3beee7e0b5c82103248c
Afshin <afshin.talebi@gmail.com>
2017-06-25 12:48:23 +0430

    load localizations after plugins loaded

--- attach-font-awesome.php
+++ attach-font-awesome.php
@@ -10,10 +10,10 @@ Description: Simply add font-awesome library to head tag
 Author: Afshin Talebi
 Version: 0.1
 Author URI: http://afshintalebi.com/
+Text Domain: attach-font-awesome
+Domain Path: /locale
 */
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
-//load localization files
-load_plugin_textdomain('attach-font-awesome', false, basename( dirname( __FILE__ ) ) . '/locale' );
 //config data
 require(plugin_dir_path(__FILE__).'/inc/data.php');
 //admin functions
@@ -25,7 +25,8 @@ if ( is_admin() ){
   // admin actions
   add_action( 'admin_init', 'atchfa_register_options');
   add_action( 'admin_menu', 'atchfa_plugin_options');
-  add_action('admin_enqueue_scripts','atchfa_admin_js_files'); 
+  add_action('admin_enqueue_scripts','atchfa_admin_js_files');
+  add_action('plugins_loaded', 'atchfa_load_textdomain');
 } else {
   // front actions
   add_action('wp_head','atchfa_front_css_files'); 
